eab370f9fa0928a9ff8dfa827b1e668764f9aa76
hutuqiang <2608277545@qq.com>
2015-10-30 11:18:32 +0800

    版本12
    好项目

---
+++ Day17GitHub项目/Day17GitHub项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day17GitHub项目
+//
+//  Created by tarena on 15/10/30.
+//  Copyright © 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
